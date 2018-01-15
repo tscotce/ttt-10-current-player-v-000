a9f568594b3ce4445f9f520bd6f265f6447ed933
Tatyana Scotce <tscotce@gmail.com>
2018-01-15 19:37:57 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   board.each do |element|
     counter = 0 
-    if board == "X" || board == "O"
+    if element == "O" || element == "X"
       counter += 1 
     end
     return counter.to_i 
