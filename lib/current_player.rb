a615d04069300838e9f9e39555c05cd7196ac45e
Tatyana Scotce <tscotce@gmail.com>
2018-01-15 20:01:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,17 @@
 def turn_count(board)
   counter = 0 
-  board.each do 
+  board.each do |element| 
     if board != " " 
-      
     end
     counter += 1 
     return counter.to_i 
   end
+end
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+counter = 0 
+board.each do |element|
+  puts "This is loop number #{counter}."
+  puts "This element is #{element}."
+  counter +=1 
 end
