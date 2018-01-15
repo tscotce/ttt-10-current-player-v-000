5f3bbc41ea5020946e38d3410f73a0baa9e47c9d
Tatyana Scotce <tscotce@gmail.com>
2018-01-15 19:26:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   # board.each do |element|
     counter = 0 
     if element = "X" || element = "O"
-      counter +=
+      counter += 1 
     end
   #  puts "You have made #{counter} turns."
   # end
